33d0413abfc26a95c06b5084d2244f17a1bd5511
linikerj <linikerenem@gmail.com>
2022-04-11 19:15:26 -0300

    adicionando data em aulas

--- desafios/desafio_2.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-""" Exercio 2: Fazer um programa para ler a duracao de tempo em segundos,
-dai imprimir na tela esta duração no formato horas:minutos:segundos.
-
-"""
-
-seconds = input("Digite o valor em segundos : ")
-print(datetime.timedelta(seconds=int(seconds))) # strftime -> converte a string em 
